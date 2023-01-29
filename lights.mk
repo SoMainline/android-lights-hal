f5e5a0afa6320b517efb42bda7d5bb266b809deb
rsglobal <36298915+rsglobal@users.noreply.github.com>
2023-01-29 23:56:51 +0200

    Update lights.mk

--- lights.mk
+++ lights.mk
@@ -2,4 +2,9 @@ PRODUCT_PACKAGES += \
 	android.hardware.lights-service
 
 # TODO: LOCAL_PATH doesn't work here
-BOARD_VENDOR_SEPOLICY_DIRS += device/mainline/common/lights/sepolicy
+
+# How about
+ALH_PATH := $(patsubst $(CURDIR)/%,%,$(dir $(abspath $(lastword $(MAKEFILE_LIST)))))
+
+BOARD_VENDOR_SEPOLICY_DIRS += $(ALH_PATH)/sepolicy
+# ?
